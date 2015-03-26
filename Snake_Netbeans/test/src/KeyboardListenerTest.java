4501670d80f5292edf7781809e085dac8a929492
Tara <earlybird87@gmail.com>
2015-03-26 13:18:48 +0200

    Adding tests

---
+++ Snake_Netbeans/test/src/KeyboardListenerTest.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src;
+
+import java.awt.Button;
+import java.awt.event.KeyEvent;
+import javax.swing.JPanel;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Tarleena_2
+ */
+public class KeyboardListenerTest {
+    
+    public KeyboardListenerTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of keyPressed method, of class KeyboardListener.
+     */
+    @Test
+    public void testKeyPressed1() {
+        System.out.println("keyPressed - Right");
+        ThreadsController.directionSnake = 3;
+        Button a = new Button("click");
+        KeyEvent e;
+        e = new KeyEvent(a, 1, 20, 1, 10, 'a');
+        e.setKeyCode(39);
+        KeyboardListener instance = new KeyboardListener();
+        instance.keyPressed(e);
+        assertEquals(1, ThreadsController.directionSnake);
+    }
+        /**
+     * Test of keyPressed method, of class KeyboardListener.
+     */
+    @Test
+    public void testKeyPressed2() {
+        System.out.println("keyPressed - Left");
+        ThreadsController.directionSnake = 4;
+        Button a = new Button("click");
+        KeyEvent e;
+        e = new KeyEvent(a, 1, 20, 1, 10, 'a');
+        e.setKeyCode(37);
+        KeyboardListener instance = new KeyboardListener();
+        instance.keyPressed(e);
+        assertEquals(2, ThreadsController.directionSnake);
+    }
+}
---
+++ Snake_Netbeans/test/src/ThreadsControllerTest.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Tarleena_2
+ */
+public class ThreadsControllerTest {
+    
+    public ThreadsControllerTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of run method, of class ThreadsController.
+     */
+    @Test
+    public void testRun() {
+        System.out.println("run");
+        Tuple pd = new Tuple(0,0);
+        ThreadsController instance = new ThreadsController(pd);
+        instance.run();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
